f7f51bb94dd5d9fb5da60f9c78a1dd29eb8dea6b
Islam Yamor <islamyamor@protonmail.com>
2023-12-12 21:34:38 +0300

    built the project

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,2 +1,4 @@
 export { default } from './stack-player';
+export * from './stack-player';
+export * from './types';
 //# sourceMappingURL=index.d.ts.map
--- @types/stack-player.d.ts
+++ @types/stack-player.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="qs" />
 import type { Options, StackType } from 'types';
 import { RequestHandler } from 'express';
-export declare function stackPlayer(stack: StackType, options?: Options): RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
-export default stackPlayer;
+export declare function stackplayer(stack: StackType, options?: Options): RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
+export default stackplayer;
 //# sourceMappingURL=stack-player.d.ts.map
